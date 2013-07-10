cb337b79a0ce1b0f9c8fd9315de3a74aa02e9894
Ilia Choly <icholy@accipiterradar.com>
2013-07-10 13:10:21 -0400

    Change slice declaration

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ type BreakPoint struct {
 
 func ParseLine(line string) *BreakPoint {
 	match := r.FindStringSubmatch(line)
-	cmds := make([]string, 0, 1)
+  var cmds []string
 	if len(match[2]) > 0 {
 		for _, cmd := range strings.Split(match[2], ";") {
 			cmd := strings.Trim(cmd, " ")
