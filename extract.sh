89da74721e4354123862aeecdf867134be2071cf
Ilia Choly <icholy@accipiterradar.com>
2013-08-30 10:22:02 -0400

    add other debug func

--- extract.sh
+++ extract.sh
@@ -1,13 +1,5 @@
-gow () {
-  while true; do
-    clear
-    echo "go $@"
-    go "$@"
-    if [ $? -eq 0 ]; then break; fi
-    inotifywait -e modify *.go 2> /dev/null
-  done
-}
 
+# debug application
 gdbb () {
 
   # build with debug flags
@@ -27,4 +19,27 @@ gdbb () {
 
   # clean up
   rm .breakpoints out
+}
+
+# debug tests
+gdbbtest () {
+
+  # build with debug flags
+  go test -c -gcflags "-N -l" "$@"
+
+  # make sure the build didn't fail
+  if [ $? != 0 ]; then return; fi
+
+  # extract debugger comments
+  gdbb-extract "*.go" > .breakpoints
+
+  # if breakpoints were found, run on start
+  if [ -s .breakpoints ]; then
+    gdb -x .breakpoints -ex run *.test
+  else
+    gdb *.test
+  fi
+
+  # clean up
+  rm .breakpoints *.test
 }
